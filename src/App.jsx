99021a9d0e046cdc98f13d5c03db83a551bc2bf1
patrick-fv <patrick.fv@outlook.com>
2020-10-29 01:14:24 -0300

    implement Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
 import React from 'react'
-import { useGet, usePost, useDelete } from './utils'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Rest from './utils/rest'
+import { Home, Movements } from './pages'
 import { Header } from './elements'
 
-const urlDataBase = 'https://mymoney-a586c.firebaseio.com/movement/2020-10.json'
+const baseURL = 'https://mymoney-a586c.firebaseio.com/'
+const { useGet, usePost, useDelete } = Rest(baseURL)
 
 const App = () => {
-  const data = useGet(urlDataBase)
+  const data = useGet('months')
   const data2 = useGet('https://httpbin.org/ip')
-  const [postData, post] = usePost(urlDataBase)
+  const [postData, post] = usePost('movement/2020-10')
   const [deleteData, remove] = useDelete()
 
   const save = () => {
-    post({
-      describe: 'almoço',
-      value: '12,50'
-    })
+  }
+
+  const doRemove = () => {
   }
 
   return (
-    <div>
-      <Header />
-      { data.loading && <p>Loading...</p> }
-      { JSON.stringify(data.data) }
-      <button onClick={save}>Salvar</button>
-       <pre>{ JSON.stringify(data2) }</pre>
-    </div>
+    <BrowserRouter>
+    <Header />
+      <div className="container">
+        <Route path="/" exact component={Home} />
+        <Route path="/movimentacoes/:data" exact component={Movements} />
+      </div>
+    </BrowserRouter>
   )
 }
 
